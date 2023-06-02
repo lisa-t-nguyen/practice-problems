1062d14ffc67a5a3615a43bf3b085490e53b2b88
Lisa Nguyen <lisanguyensc@gmail.com>
2023-06-02 13:38:10 +0000

    Pseudocode for checkPalindrome algo

--- practice-problems/checkPalindrome.js
+++ practice-problems/checkPalindrome.js
@@ -9,3 +9,19 @@ function solution(string) {
 }
 
 solution('asdfe');
+// "aba"
+// const stringLength = 3;
+// LOOP
+// stringLength / 2 = 1.5
+// i = 0, is 0 < 1.5? YES
+// string[i(0)] = "a"
+// string[stringLength - 1 - i(0)] = "a"
+// "a" === "a" TRUE, so continue looping
+// LOOP
+// i = 1, is 1 < 1.5? YES
+// string[i(1)] = "b"
+// string[stringLength - 1 - i(1)] = "b"
+// "b" === "b" TRUE, so continue looping
+// LOOP
+// i = 2, is 2 < 1.5? NO, stop looping
+// return true
