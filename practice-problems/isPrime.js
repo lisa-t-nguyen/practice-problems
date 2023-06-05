51e257d756f183c5189189272593139f2e787aea
Lisa Nguyen <lisanguyensc@gmail.com>
2023-06-05 20:10:47 +0000

    Added isPrime Algo

---
+++ practice-problems/isPrime.js
@@ -0,0 +1,10 @@
+function isPrime(number) {
+  if (number < 2) return false;
+
+  for (let i = 2; i <= Math.sqrt(number); i += 1) {
+    if (number % i === 0) return false;
+  }
+  return true;
+}
+
+isPrime(5);
