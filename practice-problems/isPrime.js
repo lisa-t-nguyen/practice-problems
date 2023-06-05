e3aa66988265d0cd4548b4f27703f4f50e6fbe08
Lisa Nguyen <lisanguyensc@gmail.com>
2023-06-05 21:49:26 +0000

    Added isPrime pseudocode

--- practice-problems/isPrime.js
+++ practice-problems/isPrime.js
@@ -1,10 +1,24 @@
 function isPrime(number) {
-  if (number < 2) return false;
+  if (number < 2) return false; // if the number is less than 2, return false (1 is not a prime number)
 
-  for (let i = 2; i <= Math.sqrt(number); i += 1) {
-    if (number % i === 0) return false;
+  for (let i = 2; i <= Math.sqrt(number); i++) { // iterate through up to and including square root of input number
+    if (number % i === 0) return false; // if the input number divided by i has a remainder of 0, return false (it is not prime)
   }
-  return true;
+  return true; // return true (it is prime)
 }
 
 isPrime(5);
+
+// 20
+// =>
+// 2 * 10
+// 2 * 2 * 5
+
+// 64
+// =>
+// 2 * 32
+// 4 * 16
+// 8 * 8
+
+// 640000 / 2 = 320000
+// sqrt(640000) = 800
