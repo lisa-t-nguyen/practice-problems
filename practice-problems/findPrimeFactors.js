469bb9aa58500759ae120fe7428fc7cad057b59f
Lisa Nguyen <lisanguyensc@gmail.com>
2023-06-05 21:38:55 +0000

    Added findPrimeFactors and sortedPrimes pseudo code

--- practice-problems/findPrimeFactors.js
+++ practice-problems/findPrimeFactors.js
@@ -1,23 +1,24 @@
 function findPrimeFactors(number) {
-  const factors = [];
-  let divisor = 2;
+  const factors = []; // initialize a constant empty array factors
+  let divisor = 2; // initialize a divisor starter at 2
 
-  if (number === 2) {
-    return [2];
+  if (number === 2) { // if the number is already 2
+    return [2]; // return an array that only includes a factor of 2
   }
 
-  while (number > 2) {
+  while (number > 2) { // while the number is greater than 2
+    // if the divisor divides the number with no remainder (if there is a remainder that is not 0, then the divisor is not a factor)
     if (number % divisor === 0) {
-      if (!factors.includes(divisor)) {
-        factors.push(divisor);
-      }
-      number = number / divisor;
-    } else {
-      divisor++;
+      if (!factors.includes(divisor)) { // if the factors array doesn't have the divisor already
+        factors.push(divisor); // push the divisor into the factors array
+      } // this prevents any duplicate divisors from being pushed (uniques only)
+      number = number / divisor; // then divide the number by the factor we have found, setting this smaller number to the number variable.
+    } else { // otherwise, if the divisor was not a factor
+      divisor++; // increment the divisor by 1
     }
   }
 
-  return factors;
+  return factors; // return the factors array containing all of the unique prime factors of the number
 }
 
 findPrimeFactors(5);
--- practice-problems/sortedPrimes.js
+++ practice-problems/sortedPrimes.js
@@ -1,24 +1,24 @@
 import isPrime from 'isPrime.js';
 
 function sortedPrimes(array) {
-  const result = [];
-  for (let i = 0; i < array.length; i++) {
-    if (isPrime(array[i])) {
-      result.push(array[i]);
+  const result = []; // initialize an empty array result to be returned
+  for (let i = 0; i < array.length; i++) { // looping through input array
+    if (isPrime(array[i])) { // if number at array[i] is prime
+      result.push(array[i]); // push it into the result array
     }
   }
 
-  result.sort((a, b) => {
-    if (a > b) {
-      return 1;
+  result.sort((a, b) => { // sort the array of numbers in ascending order
+    if (a > b) { // if a is greater than b
+      return 1; // return 1, meaning a goes before b
     }
-    if (a < b) {
-      return -1;
+    if (a < b) { // if a is less than b
+      return -1; // return -1, meaning a goes after b
     }
-    return 0;
+    return 0; // if a is neither of the above, we know it's equal to b. Return 0 meaning their the same.
   });
 
-  return result;
+  return result; // return the result array
 }
 
 sortedPrimes([7, 5, 9, 1, 4, 11]);
